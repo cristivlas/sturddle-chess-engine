5c028acf0a80b642bf400c19766b9e6661dba694
Cristian Vlasceanu <cvlasceanu@tableau.com>
2022-04-20 19:37:09 -0700

    passed pawn eval fix

--- context.cpp
+++ context.cpp
@@ -1133,7 +1133,7 @@ namespace search
             });
         }
 
-        return score + eval_passed_formation(state, piece_count);
+        return score;
     }
 
 
@@ -1211,8 +1211,11 @@ namespace search
 
         eval += eval_material_imbalance(state, piece_count, mat_eval);
         eval += eval_open_files(state, piece_count);
+
         eval += eval_passed_pawns<0>(state, piece_count);
         eval += eval_passed_pawns<1>(state, piece_count);
+        eval += eval_passed_formation(state, piece_count);
+
         eval += eval_pawn_structure(state, piece_count);
         eval += eval_piece_grading(state, piece_count);
 
