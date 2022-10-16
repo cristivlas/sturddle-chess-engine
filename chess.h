bc9da655fa08cf11e4763c67a8bc8fe08c8b0077
Cristian <cristi.vlasceanu@gmail.com>
2022-10-16 00:23:11 -0700

    INLINE-d estimate_captures

--- chess.h
+++ chess.h
@@ -1647,7 +1647,27 @@ namespace chess
 
     /* SEE captures.cpp */
     score_t estimate_static_exchanges(const State&, Color, Square, PieceType = PieceType::NONE);
-    score_t estimate_captures(const State&);
+
+    INLINE score_t estimate_captures(const State& board)
+    {
+        score_t value = 0;
+        const auto color = board.turn;
+
+        for (auto piece_type : {QUEEN, ROOK, BISHOP, KNIGHT, PAWN})
+        {
+            if (value >= board.weight(piece_type))
+                break;
+
+            const auto victims = board.pieces_mask(piece_type, Color(color ^ 1));
+
+            for_each_square(victims, [&](Square square)
+            {
+                auto v = estimate_static_exchanges(board, color, square, piece_type);
+                value = std::max(value, v);
+            });
+        }
+        return value;
+    }
 
 
     /*
