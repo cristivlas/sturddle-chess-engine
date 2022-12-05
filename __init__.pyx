dca93ab85ac60a13101d729ac85e26962f3d831a
Cristian <cristian@Sturddle.local>
2022-12-04 23:27:34 -0800

    tuneup/config

--- __init__.pyx
+++ __init__.pyx
@@ -1366,7 +1366,7 @@ nnue_init(os.path.dirname(__file__))
 _tb_init()
 
 __major__   = 1
-__minor__   = 14
+__minor__   = 15
 __build__   = ['NNUE', str(__major__), f'{int(__minor__):02d}', timestamp().decode()]
 
 
